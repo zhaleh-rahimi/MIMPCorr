350e0de44b525cdd3e65bc58e1192cff938db936
zhaleh-rahimi <rhmi.zhle@gmail.com>
2025-12-15 00:35:37 -0500

    updated

--- synthetic.py
+++ synthetic.py
@@ -6,13 +6,9 @@ Created on Thu Jan  9 12:18:18 2025
 """
 from evaluation.percentage_improvement_reports import run_config_directory, run_config_file, delta_c_gain_single_run
 from evaluation.param_estimation_err_impact import delta_c_est_single_run, run_config_directory as run_dir_est ,run_config_file as run_file_est
-from evaluation.armaEst_vs_varmaTrue import armaEst_varmaTrue_single_run, run_config_directory as run_dir_arma_est, run_config_file as run_file_arma_est
 from evaluation.misspec_sensitivity import misspec_batch_run
-from evaluation.train_size_sensitivity import train_size_sensitivity_batch_run
-from evaluation.noise_sensitivity import noise_level_sensitivity_batch_run
-from evaluation.sub_cost_analysis import cost_analysis_batch_run
 from evaluation.controlled_est_error import estimation_error_batch_run
-
+from evaluation.sub_cost_analysis import cost_analysis_batch_run
 import argparse
 
 
@@ -24,30 +20,22 @@ if __name__ == "__main__":
     # Directory/File path
     directory_path = "inputs/json"
 
-    file_path = "inputs/json/6items_2-1_1_10.json"
+    file_path = "inputs/json/2items_4-1_1_10.json"
 
     # Delta C gain (VARMA vs ARMA inventory performance)
     # run_config_directory(delta_c_gain_single_run, directory_path, n_run)
-    run_config_file(delta_c_gain_single_run, file_path, n_run)
+    # run_config_file(delta_c_gain_single_run, file_path, n_run)
     
     # Delta C est (VARMA true true vs VARMA estimated inventory performance)
-    run_dir_est(delta_c_est_single_run, directory_path, n_run)
+    # run_dir_est(delta_c_est_single_run, directory_path, n_run)
     # run_file_est(delta_c_est_single_run, file_path, n_run)
 
-    # Delta C est (VARMA true true vs ARMA estimated inventory performance)
-    run_dir_arma_est(armaEst_varmaTrue_single_run, directory_path, n_run)
-    # run_file_arma_est(armaEst_varmaTrue_single_run, file_path, n_run)
-
-
-    # noise sensitivity analysis
-    noise_results = noise_level_sensitivity_batch_run(n_run)
-
-    # Train size sensitivity analysis
-    train_size_sensitivity_batch_run(n_run)
     
     # Controlled estimation error impact analysis
-    estimation_error_batch_run(n_run)
-
+    # estimation_error_batch_run(1000)
 
     #misspec sensitivity analysis
-    misspec_batch_run(n_run)
+    # misspec_batch_run(n_run)
+
+    #plot unit and cost analysis
+    cost_analysis_batch_run(n_run)
